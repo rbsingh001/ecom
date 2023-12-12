b981334db29fff836f1ce10f2701fbd29afab598
rbsingh001 <129167801+rbsingh001@users.noreply.github.com>
2023-12-12 16:37:41 +0530

    Ecom with cart product adds in json file

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -13,7 +14,7 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId , product =>{
-    res.render('shop/product-details',{
+    res.render('shop/product-detail',{
        product: product ,
        pageTitle: product.title,
        path: '/products'
@@ -37,6 +38,15 @@ exports.getCart = (req, res, next) => {
     path: '/cart',
     pageTitle: 'Your Cart'
   });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId,product.price);
+  });
+  // console.log(prodId);
+  res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
